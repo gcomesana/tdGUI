6b572a86fe665f99786c63ffb3369330a7ac0e42
w i l l y <gcomesana@cnio.es>
2012-05-03 18:11:25 +0200

    Initial commit. Layout in progress

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TdGUI::Application.config.secret_token = 'e76b8fcc7e38a52e0ed67e012ca0dd6d4ce46735a68d6e684b85fd136d547b1dc5c3b96c22593ee295ba68baa6caa5f91fdfefaae6fc4165e833fd17c2b0da33'
